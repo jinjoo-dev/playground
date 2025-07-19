b22fb9c1c37b28849b5aba13d42b513f2bf991ae
박진주 <147597143+jinjoo-dev@users.noreply.github.com>
2025-07-20 01:00:25 +0900

    feat: lh 단위를 활용한 여러줄 말줄임 처리 (#4)
    * feat: lh 단위를 활용한 여러줄 말줄임 처리
    
    * feat: 날짜 표기
    
    * chore: readme 업데이트

--- apps/react-ui/src/app/index.tsx
+++ apps/react-ui/src/app/index.tsx
@@ -1,5 +1,12 @@
+import type { CSSProperties } from 'react';
 import './App.css';
 import { Link } from 'react-router';
+
+const dateStyle: CSSProperties = {
+  fontSize: '12px',
+  color: 'gray',
+  marginLeft: '8px',
+};
 
 function App() {
   return (
@@ -10,6 +17,13 @@ function App() {
       <ol>
         <li>
           <Link to="/ref-transition">RefTransition</Link>
+          <span style={dateStyle}>2025/07/12</span>
+        </li>
+        <li>
+          <Link to="/line-height-see-more">
+            lh 단위를 활용한 여러줄 말줄임 처리
+          </Link>
+          <span style={dateStyle}>2025/07/20</span>
         </li>
       </ol>
     </div>
--- apps/react-ui/src/app/router.tsx
+++ apps/react-ui/src/app/router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import App from '.';
-import { RefTransition } from '../pages';
+import { LineHeightSeeMore, RefTransition } from '../pages';
 
 export const Router = () => {
   return (
@@ -8,6 +8,7 @@ export const Router = () => {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/ref-transition" element={<RefTransition />} />
+        <Route path="/line-height-see-more" element={<LineHeightSeeMore />} />
       </Routes>
     </BrowserRouter>
   );
